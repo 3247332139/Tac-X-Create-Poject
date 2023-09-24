d118d56aab1709a28bf709e3764181b63abe51ef
bfybf <3247332139@qq.com>
2023-09-24 19:01:43 +0800

    delete!!!!!!!!!!!!!!!!!!

--- build/tmp/expandedArchives/forge-1.16.5-36.2.39_mapped_parchment_2021.10.17-1.16.5-sources.jar_8cb90a051e1719c238ce15036c2073de/net/minecraft/resources/IPackFinder.java
+++
@@ -1,7 +0,0 @@
-package net.minecraft.resources;
-
-import java.util.function.Consumer;
-
-public interface IPackFinder {
-   void loadPacks(Consumer<ResourcePackInfo> pInfoConsumer, ResourcePackInfo.IFactory pInfoFactory);
-}
--- build/tmp/expandedArchives/forge-1.16.5-36.2.39_mapped_parchment_2021.10.17-1.16.5-sources.jar_8cb90a051e1719c238ce15036c2073de/net/minecraft/test/TestRuntimeException.java
+++
@@ -1,7 +0,0 @@
-package net.minecraft.test;
-
-public class TestRuntimeException extends RuntimeException {
-   public TestRuntimeException(String pExceptionMessage) {
-      super(pExceptionMessage);
-   }
-}
